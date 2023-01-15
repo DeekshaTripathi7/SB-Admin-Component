209092cf393cbae60a5d4af490d0358e7249595c
DeekshaTripathi7 <deeksha18495@gmail.com>
2023-01-15 23:36:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import './css/Users.css';
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <Sidebar />
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+
+export const Sidebar = () => {
+    return (
+        <div id="page-top">
+            <div id="wrapper">
+
+                {/* <!-- Sidebar --> */}
+                <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                    {/* <!-- Sidebar - Brand --> */}
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon rotate-n-15">
+                            <i className="fas fa-laugh-wink"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                    </a>
+                    {/* <!-- Divider --> */}
+                    <hr class="sidebar-divider my-0"></hr>
+                </ul>
+            </div>        
+
+        </div>
+    )
+}
